6d6b62065b53156dc1076e0a6a6708741ec41eef
douglasWarren <douglas.fidelis@warren.com.br>
2022-09-20 13:09:54 -0300

    feat: created coin gencko api urls

---
+++ lib/app/data/datasources/remote_datasource/endpoints/coingencko.dart
@@ -0,0 +1,5 @@
+class Coingencko {
+  static String baseUrl = "https://api.coingecko.com/api/v3";
+  static String getAllCoins = "$baseUrl/list";
+  static String getCoinById(String coinId) => "/coins/$coinId/tickersL";
+}
