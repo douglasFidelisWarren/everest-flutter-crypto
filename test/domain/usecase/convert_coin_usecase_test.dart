1dc3241eaf557f48141d72fbe1dff2a755184d44
douglasWarren <douglas.fidelis@warren.com.br>
2022-10-10 14:27:16 -0300

    test: usecases coverage 45.7

---
+++ test/domain/usecase/convert_coin_usecase_test.dart
@@ -0,0 +1,34 @@
+import 'package:decimal/decimal.dart';
+import 'package:everest_crypto/app/domain/entities/exchange_entity.dart';
+import 'package:everest_crypto/app/domain/usecases/convert_coin_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../helpers/fake_repo.dart';
+
+void main() {
+  late ConvertCoinUsecaseImp convertCoinUsecase;
+  late FakeRepo repo;
+  late ExchangeEntity exchangeEntity;
+
+  setUp(() {
+    convertCoinUsecase = ConvertCoinUsecaseImp();
+    repo = FakeRepo();
+    exchangeEntity = convertCoinUsecase.convertCoin(
+      fromCoin: repo.getCoin(),
+      toCoin: repo.getCoin(),
+      amtConvert: Decimal.parse("1"),
+    );
+  });
+
+  test(
+    "WHEN convertCoin called THEN returns ExchangeEntity",
+    () {
+      final entity = convertCoinUsecase.convertCoin(
+        fromCoin: repo.getCoin(),
+        toCoin: repo.getCoin(),
+        amtConvert: Decimal.parse("1"),
+      );
+      expect(entity.amtConvert, exchangeEntity.amtConvert);
+    },
+  );
+}
