013877347689550dc94f6be93f6126600f441809
douglasWarren <douglas.fidelis@warren.com.br>
2022-10-13 16:46:46 -0300

    test: project coverage test 95.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/presenter/ui/shared/app_routes.dart';
+import 'app/presenter/ui/conversion/view/success_page.dart';
+import 'app/presenter/ui/movements/view/movements_page.dart';
+import 'app/presenter/ui/portfolio/view/portfolio_page.dart';
+import 'app/presenter/ui/review/view/review_page.dart';
 import 'app/presenter/ui/shared/home.dart';
 import 'l10n/core_strings.dart';
 
@@ -24,7 +27,13 @@ void main() {
         )),
         debugShowCheckedModeBanner: false,
         initialRoute: Home.route,
-        routes: appRoutes,
+        routes: {
+          Home.route: (context) => const Home(),
+          PortfolioPage.route: (context) => const PortfolioPage(coins: []),
+          MovementsPage.route: (context) => const MovementsPage(),
+          ReviewPage.route: (context) => const ReviewPage(),
+          SuccessPage.route: (context) => const SuccessPage(),
+        },
       ),
     ),
   );
