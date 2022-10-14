f4ccc3b37f364f02b57846a8b5d1090645f91791
douglasWarren <douglas.fidelis@warren.com.br>
2022-10-14 09:42:43 -0300

    resolved pull request conversation

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,18 +1,12 @@
 import 'package:everest_crypto/app/domain/entities/coins_view_data.dart';
-import 'package:everest_crypto/app/presenter/ui/portfolio/widgets/page_body.dart';
 import 'package:everest_crypto/app/presenter/ui/review/view/review_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../app/presenter/controllers/providers/providers.dart';
-import '../app/presenter/ui/conversion/view/conversion_page.dart';
 import '../app/presenter/ui/conversion/view/success_page.dart';
 import 'home.dart';
 import '../app/presenter/ui/movements/view/movements_page.dart';
 import '../app/presenter/ui/portfolio/view/portfolio_page.dart';
-
-// final coinlistProvider = Provider<List<CoinViewData>>(
-//     (ref) => ref.watch(coinsWalletProvider).value!);
-// final coinlist = coinlistProvider as List<CoinViewData>;
 
 final coinProvider = Provider<CoinViewData>(
   (ref) => ref.watch(detailCoinProvider.state).state,
@@ -23,8 +17,6 @@ var appRoutes = {
   Home.route: (context) => const Home(),
   PortfolioPage.route: (context) => const PortfolioPage(coins: []),
   MovementsPage.route: (context) => const MovementsPage(),
-  //DetailsPage.route: (context) => DetailsPage(coin: coin),
-  //ConversionPage.route: (context) => const ConversionPage(),
   ReviewPage.route: (context) => const ReviewPage(),
   SuccessPage.route: (context) => const SuccessPage(),
 };
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:everest_crypto/app/presenter/ui/details/view/details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/presenter/controllers/providers/get_coins_wallet_provider.dart';
 import 'core/app_routes.dart';
 import 'core/home.dart';
 import 'l10n/core_strings.dart';
